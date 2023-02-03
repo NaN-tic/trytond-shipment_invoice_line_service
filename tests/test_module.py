5d5eecc9ef0fc0ec628af3e055066532143fab77
juanjo-nan <57089222+juanjo-nan@users.noreply.github.com>
2023-02-03 12:10:11 +0100

    Move functionality from https://github.com/NaN-tic/trytond-helsa/commit/907c5c68e78cf66abd28a6809203c07087c5c050 into a new module. (#1)
    Task #072467

---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part shipment_invoice_line_service module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class ShipmentInvoiceLineServiceTestCase(ModuleTestCase):
+    'Test Shipment Invoice Line Service module'
+    module = 'shipment_invoice_line_service'
+
+del ModuleTestCase
